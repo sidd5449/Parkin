74ccd834a314f97a06bad7c6319ea1e62c580c7c
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2024-03-13 01:15:33 +0530

    SpotCard component completed

--- frontend/src/components/SpotCard/SpotCard.jsx
+++ frontend/src/components/SpotCard/SpotCard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './SpotCard.scss';
 
-const SpotCard = ({props}) => {
+const SpotCard = ( props ) => {
+  console.log(props);
   return (
     <div className="app__spotcard">
-        
+      <h2>{props.spotTitle}</h2>
+      <p>{props.spotAddress}</p>
+      <div className="app__spotcard-vehinfo">
+        <div className="vehinfo-fours">
+          <p>4-wheeler: </p>
+          <p>{props.fours}</p>
+        </div>
+        <div className="vehinfo-twos">
+          <p>2-wheeler: </p>
+          <p>{props.twos}</p>
+        </div>
+      </div>
+      <hr />
     </div>
   )
 }
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import SpotCard from '../../components/SpotCard/SpotCard';
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <SpotCard spotTitle="Lorem Ipsum" spotAddress="Some random dummy adress which you’ll never find in this mortal world" fours={20} twos={20} />
+    </div>
   )
 }
 
