8b705f08f6b01043a2a702e9ccefc9ff1b06c5e1
Siddhesh Thakare <76845396+sidd5449@users.noreply.github.com>
2024-05-06 15:32:54 +0000

    ws added

--- admin-panal/src/pages/scan-qr/ScanQR.jsx
+++ admin-panal/src/pages/scan-qr/ScanQR.jsx
@@ -4,10 +4,24 @@ import { FiMaximize } from "react-icons/fi";
 import { AiOutlineScan } from "react-icons/ai";
 import { Scanner } from "@yudiel/react-qr-scanner";
 import axios from "axios";
-
+import WebSocket from "ws";
 const ScanQR = () => {
   const [data, setdata] = useState(null);
   const [dbData, setdbData] = useState(null);
+  const sendWsId = (id) => {
+    const ws = new WebSocket("ws://localhost:8080");
+    ws.on("open", function open() {
+      console.log("Connected to WebSocket server");
+
+      // Send a message to the server
+      ws.send("Hello, server! This is the WebSocket client.");
+    });
+
+    // Event listener for messages received from the server
+    ws.on("message", function incoming(message) {
+      console.log("Received: %s", message);
+    });
+  };
   if (data !== null && dbData === null) {
     const splits = data.split(",");
     const slotId = splits[0];
