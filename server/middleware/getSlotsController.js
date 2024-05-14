2385cba34edb1b48ff7d069383d399b779673d0d
Siddhesh Thakare <76845396+sidd5449@users.noreply.github.com>
2024-05-14 20:20:00 +0530

    Update getSlotsController.js

--- server/middleware/getSlotsController.js
+++ server/middleware/getSlotsController.js
@@ -4,7 +4,7 @@ export const getSlotsController = async (req, res) => {
   try {
     const { id } = req.params;
     console.log(id);
-    const slotsData = await slotData.find({ spot: id });
+    const slotsData = await slotData.find({ spot: id }).sort({id : 1});
     res.status(200).json(slotsData);
   } catch (error) {
     res.status(404).json({ message: error.message });
